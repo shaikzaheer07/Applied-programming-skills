ebd8d67dc1225d1179e1dc4cd3c3c0a0facd418b
shaik zaheer <skzaheer750@gmail.com>
2026-04-08 09:17:18 +0530

    Time: 109 ms (57.23%) | Memory: 57.5 MB (81.57%) - LeetSync

--- 1512-design-underground-system/design-underground-system.java
+++ 1512-design-underground-system/design-underground-system.java
@@ -2,53 +2,55 @@ import java.util.*;
 
 class UndergroundSystem {
 
-   
-    private Map<Integer, Pair> checkInMap;
-
-    
-    private Map<String, int[]> travelMap;
-
-    public UndergroundSystem() {
-        checkInMap = new HashMap<>();
-        travelMap = new HashMap<>();
-    }
-
-    
-    class Pair {
+    class CheckIn {
         String station;
         int time;
 
-        Pair(String station, int time) {
+        CheckIn(String station, int time) {
             this.station = station;
             this.time = time;
         }
     }
 
+    class Trip {
+        int totalTime;
+        int count;
+
+        Trip(int totalTime, int count) {
+            this.totalTime = totalTime;
+            this.count = count;
+        }
+    }
+
+    Map<Integer, CheckIn> checkInMap;
+    Map<String, Trip> tripMap;
+
+    public UndergroundSystem() {
+        checkInMap = new HashMap<>();
+        tripMap = new HashMap<>();
+    }
+
     public void checkIn(int id, String stationName, int t) {
-        checkInMap.put(id, new Pair(stationName, t));
+        checkInMap.put(id, new CheckIn(stationName, t));
     }
 
     public void checkOut(int id, String stationName, int t) {
-        Pair checkIn = checkInMap.get(id);
-
-        String startStation = checkIn.station;
-        int startTime = checkIn.time;
-
-        String route = startStation + "->" + stationName;
-        int travelTime = t - startTime;
-
-        travelMap.putIfAbsent(route, new int[2]);
-
-        travelMap.get(route)[0] += travelTime;  
-        travelMap.get(route)[1] += 1;          
-
+        CheckIn ci = checkInMap.get(id);
         checkInMap.remove(id);
+
+        String key = ci.station + "-" + stationName;
+        int time = t - ci.time;
+
+        Trip trip = tripMap.getOrDefault(key, new Trip(0, 0));
+        trip.totalTime += time;
+        trip.count += 1;
+
+        tripMap.put(key, trip);
     }
 
     public double getAverageTime(String startStation, String endStation) {
-        String route = startStation + "->" + endStation;
-        int[] data = travelMap.get(route);
-
-        return (double) data[0] / data[1];
+        String key = startStation + "-" + endStation;
+        Trip trip = tripMap.get(key);
+        return (double) trip.totalTime / trip.count;
     }
 }
